023d587dcce5837f2386d9f742a4799ad639505c
brianc2723 <108293296+brianc2723@users.noreply.github.com>
2023-04-24 23:51:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,58 +1,71 @@
 import streamlit as st
-import yfinance as yf
 import pandas as pd
-import matplotlib.pyplot as plt
+import yfinance as yf
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 
-st.title('Stock Comparison App')
 
-# User input
-ticker1 = st.text_input('Enter the first ticker:')
-ticker2 = st.text_input('Enter the second ticker:')
-time_frame = st.selectbox('Select the time frame:', ('日K线', '月K线', '年K线'))
+class StockWatcher(FileSystemEventHandler):
+    def __init__(self):
+        self.df1 = None
+        self.df2 = None
 
-# Retrieve data from Yahoo finance
-start_date = '2010-01-01'
-end_date = '2023-4-23'
+    def on_modified(self, event):
+        self.load_data()
 
-data1 = yf.download(ticker1, start=start_date, end=end_date)
-data2 = yf.download(ticker2, start=start_date, end=end_date)
+    def load_data(self):
+        try:
+            ticker1 = 'BRK.B'
+            ticker2 = 'TSLA'
+            df1 = yf.download(ticker1, period='max')
+            df2 = yf.download(ticker2, period='max')
+            if not df1.empty and not df2.empty:
+                self.df1 = df1['Close']
+                self.df2 = df2['Close']
+        except:
+            pass
 
-df1 = pd.DataFrame(data1['Adj Close'])
-df2 = pd.DataFrame(data2['Adj Close'])
 
-# Resample data based on selected time frame
-if time_frame == '日K线':
-    df1.index = pd.to_datetime(df1.index)
-    df2.index = pd.to_datetime(df2.index)
-elif time_frame == '月K线':
-    df1.index = pd.to_datetime(df1.index).to_period('M')
-    df2.index = pd.to_datetime(df2.index).to_period('M')
-else:
-    df1.index = pd.to_datetime(df1.index).to_period('Y')
-    df2.index = pd.to_datetime(df2.index).to_period('Y')
+def main():
+    # Watch file changes
+    observer = Observer()
+    event_handler = StockWatcher()
+    observer.schedule(event_handler, '.', recursive=False)
+    observer.start()
 
-df1 = df1.mean(level=0)
-df2 = df2.mean(level=0)
+    # Load initial data
+    event_handler.load_data()
 
-# Compare stock price trends
-st.write('### Stock Price Comparison')
-fig, ax = plt.subplots()
-ax.plot(df1.index, df1.values, label=ticker1)
-ax.plot(df2.index, df2.values, label=ticker2)
-ax.set_xlabel('Year')
-ax.set_ylabel('Adjusted Close Price ($)')
-ax.legend()
-st.pyplot(fig)
+    # Streamlit app
+    st.title('Stock Comparison')
 
-# Calculate and compare stock returns
-returns1 = df1.pct_change()
-returns2 = df2.pct_change()
+    if event_handler.df1 is None or event_handler.df2 is None:
+        st.write('Loading data...')
+        return
 
-st.write('### Stock Return Comparison')
-fig, ax = plt.subplots()
-ax.plot(returns1.index, returns1.values, label=ticker1)
-ax.plot(returns2.index, returns2.values, label=ticker2)
-ax.set_xlabel('Year')
-ax.set_ylabel('Return')
-ax.legend()
-st.pyplot(fig)
+    # Choose time frame
+    time_frame = st.selectbox('Time Frame', ['日K线', '月K线', '年K线'])
+
+    # Resample data according to time frame
+    if time_frame == '日K线':
+        df1 = event_handler.df1.resample('D').mean().dropna()
+        df2 = event_handler.df2.resample('D').mean().dropna()
+    elif time_frame == '月K线':
+        df1 = event_handler.df1.resample('M').mean().dropna()
+        df2 = event_handler.df2.resample('M').mean().dropna()
+    else:
+        df1 = event_handler.df1.resample('Y').mean().dropna()
+        df2 = event_handler.df2.resample('Y').mean().dropna()
+
+    # Compare stock prices
+    comparison = df1.to_frame(name='BRK.B').join(df2.to_frame(name='TSLA')).dropna()
+    comparison['BRK.B/TSLA'] = comparison['BRK.B'] / comparison['TSLA']
+
+    # Show comparison chart
+    st.line_chart(comparison[['BRK.B', 'TSLA']])
+    st.line_chart(comparison['BRK.B/TSLA'])
+
+
+if __name__ == '__main__':
+    main()
+
