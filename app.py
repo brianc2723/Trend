7ffc01843a322328123a6681945fa3b3b21e2104
brianc2723 <108293296+brianc2723@users.noreply.github.com>
2023-04-24 23:28:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,42 +1,49 @@
+import yfinance as yf
 import streamlit as st
 import pandas as pd
-import yfinance as yf
+import matplotlib.pyplot as plt
 
-st.set_page_config(page_title="Stock Comparison App")
+# 设置页面标题
+st.title('Stock Comparison App')
 
-st.title("Stock Comparison App")
+# 添加用户输入
+col1, col2 = st.beta_columns(2)
+with col1:
+    stock1 = st.text_input('请输入第一支股票代码（例如AAPL）：')
+with col2:
+    stock2 = st.text_input('请输入第二支股票代码（例如AMZN）：')
 
-# 輸入股票代號
-ticker_list = []
-while True:
-    ticker = st.text_input("請輸入股票代號（例如 AAPL）：")
-    if ticker:
-        ticker_list.append(ticker.upper())
-    else:
-        break
+# 添加用户选择
+time_frame = st.radio("请选择时间周期：", ("日K线", "月K线", "年K线"))
 
-# 選擇時間區間
-time_interval = st.selectbox("請選擇時間區間：", ["1日K", "1月K", "1年K"])
+# 获取数据并绘图
+@st.cache
+def get_data(stock1, stock2):
+    ticker1 = yf.Ticker(stock1)
+    ticker2 = yf.Ticker(stock2)
+    df1 = ticker1.history(period="max")
+    df2 = ticker2.history(period="max")
+    return df1, df2
 
-# 取得收盤價數據
-df = pd.DataFrame()
-for ticker in ticker_list:
-    stock_data = yf.Ticker(ticker).history(period=time_interval.lower())
-    stock_data = stock_data[['Close']]
-    stock_data = stock_data.rename(columns={'Close': ticker})
-    df = pd.concat([df, stock_data], axis=1)
+df1, df2 = get_data(stock1, stock2)
 
-# 計算每支股票的漲跌幅
-price_diff = df.pct_change().dropna()
+if time_frame == '日K线':
+    df1 = df1.resample('D').mean().dropna()
+    df2 = df2.resample('D').mean().dropna()
+elif time_frame == '月K线':
+    df1 = df1.resample('M').mean().dropna()
+    df2 = df2.resample('M').mean().dropna()
+else:
+    df1 = df1.resample('Y').mean().dropna()
+    df2 = df2.resample('Y').mean().dropna()
 
-# 計算每支股票的總漲跌幅
-total_diff = price_diff.sum()
-
-# 計算每支股票的相對漲跌幅
-relative_diff = total_diff / len(ticker_list)
-
-# 顯示比較結果
-st.subheader("比較結果：")
-st.write(price_diff)
-st.write("每支股票的總漲跌幅：", total_diff)
-st.write("每支股票的相對漲跌幅：", relative_diff)
+if len(df1) == 0 or len(df2) == 0:
+    st.write("输入的股票代码有误，请重新输入！")
+else:
+    fig, ax = plt.subplots(figsize=(10, 5))
+    ax.plot(df1.index, df1['Close']/df1['Close'][0], label=stock1)
+    ax.plot(df2.index, df2['Close']/df2['Close'][0], label=stock2)
+    ax.set_xlabel('时间')
+    ax.set_ylabel('股票价格相对初始值的比例')
+    ax.legend(loc='best')
+    st.pyplot(fig)
