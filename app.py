378c108c267fa1d6c8042957e001fb9357152476
brianc2723 <108293296+brianc2723@users.noreply.github.com>
2023-04-24 23:16:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,48 +1,42 @@
 import streamlit as st
-import pandas_datareader as pdr
 import pandas as pd
-import datetime as dt
+import yfinance as yf
 
+st.set_page_config(page_title="Stock Comparison App")
 
-def get_last_business_day():
-    """
-    Get last business day's date
-    """
-    today = dt.date.today()
-    if today.weekday() == 5:  # Saturday
-        last_business_day = today - dt.timedelta(days=1)
-    elif today.weekday() == 6:  # Sunday
-        last_business_day = today - dt.timedelta(days=2)
+st.title("Stock Comparison App")
+
+# 輸入股票代號
+ticker_list = []
+while True:
+    ticker = st.text_input("請輸入股票代號（例如 AAPL）：")
+    if ticker:
+        ticker_list.append(ticker.upper())
     else:
-        last_business_day = today - dt.timedelta(days=1)
-    return last_business_day
+        break
 
+# 選擇時間區間
+time_interval = st.selectbox("請選擇時間區間：", ["1日K", "1月K", "1年K"])
 
-st.title('Stock Comparison App')
+# 取得收盤價數據
+df = pd.DataFrame()
+for ticker in ticker_list:
+    stock_data = yf.Ticker(ticker).history(period=time_interval.lower())
+    stock_data = stock_data[['Close']]
+    stock_data = stock_data.rename(columns={'Close': ticker})
+    df = pd.concat([df, stock_data], axis=1)
 
-# Choose stocks to compare
-tickers = st.multiselect('Choose stocks to compare', ['AAPL', 'GOOG', 'MSFT', 'AMZN'])
+# 計算每支股票的漲跌幅
+price_diff = df.pct_change().dropna()
 
-# Choose time frequency
-freq_dict = {'Daily': 'D', 'Weekly': 'W', 'Monthly': 'M'}
-freq = st.selectbox('Choose time frequency', list(freq_dict.keys()))
+# 計算每支股票的總漲跌幅
+total_diff = price_diff.sum()
 
-# Get start and end dates
-end_date = get_last_business_day()
-start_date = end_date - dt.timedelta(days=365)
+# 計算每支股票的相對漲跌幅
+relative_diff = total_diff / len(ticker_list)
 
-# Get data for selected stocks
-data = pdr.get_data_yahoo(tickers, start_date, end_date)
-
-# Select 'Adj Close' prices for each stock
-close_prices = data['Adj Close']
-
-# Resample to selected frequency
-close_prices = close_prices.resample(freq_dict[freq]).mean()
-
-# Calculate returns
-returns = close_prices.pct_change()
-
-# Create a line chart for each stock
-for ticker in tickers:
-    st.line_chart(returns[ticker])
+# 顯示比較結果
+st.subheader("比較結果：")
+st.write(price_diff)
+st.write("每支股票的總漲跌幅：", total_diff)
+st.write("每支股票的相對漲跌幅：", relative_diff)
