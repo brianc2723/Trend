a992bf02b6f4edab4fa641b7816273bbc93b89c4
brianc2723 <108293296+brianc2723@users.noreply.github.com>
2023-04-25 00:19:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,70 +1,84 @@
 import yfinance as yf
-import streamlit as st
 import pandas as pd
+import streamlit as st
+import matplotlib.pyplot as plt
+import seaborn as sns
+from datetime import datetime, timedelta
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 
-# Set page title
-st.set_page_config(page_title="Stock Comparison App")
+class Watcher:
+    def __init__(self):
+        self.observer = Observer()
 
-# Define function to get stock data
-@st.cache
-def get_data(tickers, start_date, end_date):
-    """
-    This function gets stock data from Yahoo Finance API
-    """
-    data = yf.download(tickers=tickers, start=start_date, end=end_date)
-    return data
+    def run(self):
+        event_handler = Handler()
+        self.observer.schedule(event_handler, ".", recursive=True)
+        self.observer.start()
+        try:
+            while True:
+                time.sleep(5)
+        except:
+            self.observer.stop()
+            print("Error")
 
-# Define function to plot stock data
-def plot_data(df1, df2):
-    """
-    This function plots the stock data
-    """
-    # Calculate returns
-    df1['Returns'] = df1['Close'].pct_change()
-    df2['Returns'] = df2['Close'].pct_change()
+        self.observer.join()
 
-    # Calculate cumulative returns
-    df1['Cumulative Returns'] = (1 + df1['Returns']).cumprod()
-    df2['Cumulative Returns'] = (1 + df2['Returns']).cumprod()
 
-    # Plot cumulative returns
-    fig1 = df1['Cumulative Returns'].plot(title=ticker1 + ' Cumulative Returns', label=ticker1)
-    df2['Cumulative Returns'].plot(title=ticker2 + ' Cumulative Returns', label=ticker2)
-    fig1.set_xlabel("Date")
-    fig1.set_ylabel("Cumulative Returns")
+class Handler(FileSystemEventHandler):
+    @staticmethod
+    def on_any_event(event):
+        if event.is_directory:
+            return None
 
-    # Plot daily returns
-    fig2 = df1['Returns'].plot(title=ticker1 + ' Daily Returns', label=ticker1)
-    df2['Returns'].plot(title=ticker2 + ' Daily Returns', label=ticker2)
-    fig2.set_xlabel("Date")
-    fig2.set_ylabel("Daily Returns")
+        elif event.event_type == 'created':
+            print(f"Received created event - {event.src_path}.")
+            plot_data()
 
-# Set page title
-st.title("Stock Comparison App")
 
-# Define sidebar inputs
-st.sidebar.title("Inputs")
-ticker1 = st.sidebar.text_input("Enter first ticker (e.g. BRK.B)", value="BRK.B")
-ticker2 = st.sidebar.text_input("Enter second ticker (e.g. TSLA)", value="TSLA")
-start_date = st.sidebar.date_input("Start date", value=pd.to_datetime("2020-01-01"))
-end_date = st.sidebar.date_input("End date", value=pd.to_datetime("today"))
-time_frame = st.sidebar.selectbox("Select time frame", ["日K线", "月K线", "年K线"])
+def plot_data():
+    df1 = yf.download("BRK-B", period="max")
+    df2 = yf.download("TSLA", period="max")
+    
+    df1 = df1.resample('D').last().ffill().pct_change().dropna()
+    df2 = df2.resample('D').last().ffill().pct_change().dropna()
 
-# Get stock data
-data = get_data([ticker1, ticker2], start_date, end_date)
+    sns.set(style="darkgrid", palette="bright", font_scale=1.5)
+    plt.figure(figsize=(20,10))
+    plt.plot(df1, label='BRK-B')
+    plt.plot(df2, label='TSLA')
+    plt.legend(loc='best')
+    plt.title("Daily Returns Comparison")
+    plt.xlabel('Date')
+    plt.ylabel('Daily Returns')
+    st.pyplot()
 
-# Select data based on time frame
-if time_frame == '日K线':
-    data = data.resample('D').mean().dropna()
-elif time_frame == '月K线':
-    data = data.resample('M').mean().dropna()
-else:
-    data = data.resample('Y').mean().dropna()
+    df1 = df1.resample('M').mean().dropna()
+    df2 = df2.resample('M').mean().dropna()
+    sns.set(style="darkgrid", palette="bright", font_scale=1.5)
+    plt.figure(figsize=(20,10))
+    plt.plot(df1, label='BRK-B')
+    plt.plot(df2, label='TSLA')
+    plt.legend(loc='best')
+    plt.title("Monthly Returns Comparison")
+    plt.xlabel('Date')
+    plt.ylabel('Monthly Returns')
+    st.pyplot()
 
-# Split data for each ticker
-df1 = pd.DataFrame(data[ticker1])
-df2 = pd.DataFrame(data[ticker2])
+    df1 = df1.resample('Y').mean().dropna()
+    df2 = df2.resample('Y').mean().dropna()
+    sns.set(style="darkgrid", palette="bright", font_scale=1.5)
+    plt.figure(figsize=(20,10))
+    plt.plot(df1, label='BRK-B')
+    plt.plot(df2, label='TSLA')
+    plt.legend(loc='best')
+    plt.title("Yearly Returns Comparison")
+    plt.xlabel('Date')
+    plt.ylabel('Yearly Returns')
+    st.pyplot()
 
-# Plot data
-plot_data(df1, df2)
-st.pyplot()
+if __name__ == '__main__':
+    st.set_page_config(page_title="Stock Comparison App")
+    st.title("Stock Comparison App")
+    st.write("This app compares the daily, monthly, and yearly returns of Berkshire Hathaway (BRK-B) and Tesla (TSL
